d61d6ef6eadd045be8b8dbb2d10eade9256c8b00
dennisisrl <dipa27.2009@gmail.com>
2022-09-22 12:45:02 -0500

    cambio nuevo

--- src/pages/Reportes/Reportes.js
+++ src/pages/Reportes/Reportes.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Reportes.css'
+import estadisticas from './components/estadistica.png'
 import logo from './components/logo_login.png'
+
 
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { Component } from 'react';
@@ -12,42 +14,36 @@ const Reportes = () => {
       <div className='encabezado'>
         <div className='logoprovincia'>
         <img src={logo} width={'100%'} height={'100%'}/>
-
         </div>
+
         <div className='tituloreporte'>
         <p>GOBIERNO AUTÓNOMO DESCENTRALIZADO MUNICIPAL DEL CANTÓN RIOBAMBA </p>
         <p>SUBPROCESO DE DESARROLLO INSTITUCIONAL  </p>
         <p>INDICADORES DE GESTIÓN </p>
     </div>
-
-
       </div>
-  
-
-    <br></br>
-  
-
+      <div className='botton'>
+  </div>
+ 
     <div className='divisor'>
-
-    </div>
-    <br></br>
-    <div className='menudesplegable'>
-    
-
       
     </div>
 
+    <br></br>
+    <div className='menudesplegable'>
+      <div className='minimenu'>
+      </div>
+    </div>
 
+   
     <div className='piepagina'>
       <p>MUNICIPIO DE RIOBAMBA- ESPOCH</p>
 
     </div> 
       
+
   
-  </div>
- 
-    
-    
+  </div>   
 
   );
 }
